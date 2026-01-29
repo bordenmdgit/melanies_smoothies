31965611e986562f9dbf5e08f573e3189767d90c
bordenmdgit <91629036+bordenmdgit@users.noreply.github.com>
2026-01-29 09:21:50 -0600

    Reorganize imports and restore API request
    added the api request at the bottom.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 #import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
-
 
 
 # Write directly to the app
@@ -48,3 +42,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie has been ordered, '+name_on_order ,icon="✅")
         #st.success('Your Smoothie is ordered! ', icon="✅")
+import requests
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
